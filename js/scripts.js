db55253fc041ebce3a031bf48de7adbb6cd6d86a
Luiz Otávio <luizotavio.03@outlook.com>
2023-11-17 13:53:23 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
+const userName = prompt("Qual o seu nome? ")
+
 const  valorEmDolar = prompt("Digite o valor em dolar: ");
 
 const cotacaoDoDolar = 4.86;
 
 const valorEmReal = valorEmDolar * cotacaoDoDolar;
 
-alert(`Valor final após a conversão de dolar para real é: R$ ${valorEmReal.toFixed(2)}`);
+alert(`Olá ${userName}, o valor final após a conversão de dolar para real é de: R$ ${valorEmReal.toFixed(2)}`);
